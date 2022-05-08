128aaab7d644d2f58be3c2dec00affa71283929f
MyznikovGleb <myznikovgleb@yandex.ru>
2022-05-08 22:30:20 +0300

    Allow to drag and drop todos in list

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ todoEnvCB
 todoHeader
     .addEventListener('click', todoHeaderEdit);
 
-
 /// functions
 
 // add todo
@@ -91,6 +90,9 @@ function todoAdd(event) {
 
     // add todo to list
     todoList.appendChild(todo);
+
+    // allow to pick todo
+    todoSpan.addEventListener('mousedown', todoPick);
 
     // disable todo input button 
     todoInputButton.classList.add('disable');
@@ -228,6 +230,9 @@ function todoStorageLoad(event) {
         // toggle todo
         if (todoState == 'done')
             todo.classList.toggle('done');
+
+        // allow to pick todo
+        todoSpan.addEventListener('mousedown', todoPick);
     });
 }
 
@@ -282,4 +287,154 @@ function todoInputButtonToogle(event) {
         todoInputButton.classList.remove('disable');
     }
     return;
+}
+
+// pick todo
+function todoPick(event) {
+    let todo;
+    let todoShadow;
+
+    // get picked todo
+    todo = event.target.parentElement;
+    
+    // make shadow copy of picked todo
+    todoShadow = todo.cloneNode();
+    todoShadow.innerHTML = todo.innerHTML;
+    todoShadow.classList.add('shadow');
+
+    // add shadow todo to list
+    todoList.insertBefore(todoShadow, todo);
+
+    // drag todo to pick point
+    todo.classList.add('dragged');
+    todoDragTo(todo, event.pageY);
+    
+    // listen drag and drop
+    document
+        .addEventListener('mousemove', todoDrag);
+    todo
+        .addEventListener('mouseup',   todoDrop);
+
+    return;
+}
+
+// drag todo
+function todoDrag(event) {
+    todoDragTo(event.target, event.pageY);
+    return;
+}
+
+// drag todo to Y-coordinate
+function todoDragTo(todo, Y) {
+    todo.style.top = Y - todo.offsetHeight / 2 + 'px';
+    return;
+}
+
+// drop todo
+function todoDrop(event) {
+    let todo;
+    let todoShadow;
+    let target;
+    let targetIndex;
+    let todoIndex;
+    let todoStorage;
+
+    // get todo, its shadow and target
+    todo        = event.target.parentElement;
+    todoShadow  = document.querySelector('.shadow');
+    target      = todoFromPoint(event.clientX, event.clientY);
+
+    // release todo shadow
+    todoList.replaceChild(todo, todoShadow);
+    todoShadow.remove();
+
+    // release dragging
+    todo.classList.remove('dragged');
+    todo.style.top = 'auto';
+
+    // get indices of todo and target
+    todoIndex   = 1;
+    targetIndex = 1;
+    for (let i = 1; i < todoList.childNodes.length; i++) {
+        if (todoList.childNodes[i] === todo)
+            todoIndex  = i; 
+        if (todoList.childNodes[i] === target)
+            targetIndex = i;
+    }
+
+    // drop todo right before target
+    if (todoIndex < targetIndex) {
+        // go down with swaps
+        for (let i = todoIndex; i < targetIndex; i++) {
+            let blank = document.createElement('div');
+            todoList.insertBefore(blank, todoList.childNodes[i]);
+            todoList.replaceChild(todoList.childNodes[i+2], blank);
+        }
+    }
+    else if (todoIndex > targetIndex) {
+        // go up with swaps
+        for (let i = todoIndex; i > targetIndex; i--) {
+            let blank = document.createElement('div');
+            todoList.insertBefore(blank, todoList.childNodes[i-1]);
+            todoList.replaceChild(todoList.childNodes[i+1], blank);
+        }
+    }
+    else {
+        // do nothing
+    }
+    
+    // update session storage todos
+    todoStorage = [];
+    for (let i = 1; i < todoList.childNodes.length; i++) {
+        todoStorage.push({
+            content: todoList.childNodes[i].childNodes[0].innerHTML,
+            state: (todoList.childNodes[i].classList.contains('done')) ? 'done' : 'default'
+        });
+    }
+    sessionStorage.setItem('todoStorage', JSON.stringify(todoStorage));
+
+    // stop listen drag and drop
+    document
+        .removeEventListener('mousemove', todoDrag);
+    todo
+        .removeEventListener('mouseup', todoDrop);
+
+    return;
+}
+
+// get todo from point with X,Y-coordinates
+function todoFromPoint(X, Y) {
+    let todoSpanDragged;
+    let todoDragged;
+    let todoTargetSpan;
+    let todoTarget;
+
+    // get dragged todo from point
+    todoSpanDragged = document.elementFromPoint(X, Y);
+    todoDragged     = todoSpanDragged.parentElement;
+
+    // forbid to get dragged todo from point
+    todoSpanDragged.classList.add('pointerEventsNone');
+    todoDragged.classList.add('pointerEventsNone');
+
+    // get target todo form point
+    todoTargetSpan = document.elementFromPoint(X, Y);
+
+    // allow to get dragged todo from point
+    todoSpanDragged.classList.remove('pointerEventsNone');
+    todoDragged.classList.remove('pointerEventsNone');
+    
+    // return target todo
+    if (todoTargetSpan.classList.contains('todo-span')) {
+        todoTarget = todoTargetSpan.parentElement;
+        if (todoTarget.classList.contains('shadow')) {
+            return todoDragged;
+        }
+        else {
+            return todoTarget;  
+        }
+    }
+    else {
+        return todoDragged;
+    }
 }
